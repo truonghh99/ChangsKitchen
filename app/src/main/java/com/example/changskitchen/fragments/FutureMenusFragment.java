19f18660cdb733b11a4b3b12310d3e635a1c7fd3
truonghh99 <truonghh@beloit.edu>
2021-04-19 23:06:19 -0700

    Query menu list based on current date

--- app/src/main/java/com/example/changskitchen/fragments/FutureMenusFragment.java
+++ app/src/main/java/com/example/changskitchen/fragments/FutureMenusFragment.java
@@ -2,10 +2,13 @@ package com.example.changskitchen.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +16,13 @@ import android.view.ViewGroup;
 import com.example.changskitchen.R;
 import com.example.changskitchen.adapters.MenuAdapter;
 import com.example.changskitchen.databinding.FragmentFutureMenusBinding;
+import com.example.changskitchen.models.Dish;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,10 +62,47 @@ public class FutureMenusFragment extends Fragment {
                              Bundle savedInstanceState) {
         fragmentFutureMenusBinding = FragmentFutureMenusBinding.inflate(getLayoutInflater());
         rvMenu = fragmentFutureMenusBinding.rvMenu;
+        getMenuIdList();
+
         adapter = new MenuAdapter(getContext(), menuIds);
         rvMenu.setAdapter(adapter);
         rvMenu.setLayoutManager(new GridLayoutManager(getContext(), 2));
 
         return fragmentFutureMenusBinding.getRoot();
+    }
+
+    private void getMenuIdList() {
+        String today = "20210421";
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("server/saving-data/fireblog").child("menus");
+        Query futureMenusQuery = ref.orderByKey().startAt(today);
+        futureMenusQuery.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Log.e(TAG, snapshot.toString());
+                menuIds.add(snapshot.getKey());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
