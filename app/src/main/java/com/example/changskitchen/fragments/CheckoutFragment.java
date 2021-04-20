a787654c112c1f0446ad16cc8766315ac05a390f
truonghh99 <truonghh@beloit.edu>
2021-04-20 16:06:32 -0700

    Render pricing information in checkout fragment

--- app/src/main/java/com/example/changskitchen/fragments/CheckoutFragment.java
+++ app/src/main/java/com/example/changskitchen/fragments/CheckoutFragment.java
@@ -33,11 +33,14 @@ public class CheckoutFragment extends DialogFragment {
     private static final String TAG = "CheckoutFragment";
 
     private FragmentCheckoutBinding checkoutFragment;
-    private TextView tvName;
-    private TextView tvDescription;
-    private EditText etNote;
-    private EditText etQuantity;
-    private Button btAdd;
+    private TextView tvSubtotal;
+    private TextView tvTax;
+    private EditText etTip;
+    private TextView tvTotal;
+    private Button btOrder;
+    private float tax;
+    private float tip;
+    private float finalPrice;
 
     public static DishFragment newInstance(Dish dish, String menuId) {
         DishFragment fragment = new DishFragment();
@@ -53,6 +56,21 @@ public class CheckoutFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         checkoutFragment = FragmentCheckoutBinding.inflate(getLayoutInflater());
+        tvSubtotal = checkoutFragment.tvSubtotal;
+        tvTax = checkoutFragment.tvTax;
+        etTip = checkoutFragment.etTip;
+        tvTotal = checkoutFragment.tvTotal;
+        btOrder = checkoutFragment.btOrder;
+
+        tax = (float) (CurrentOrder.totalPrice * 0.1);
+        tip = (float) ((CurrentOrder.totalPrice + tax) * 0.2);
+        finalPrice = (float) tax + tip + CurrentOrder.totalPrice;
+
+        tvSubtotal.setText(String.valueOf(CurrentOrder.totalPrice));
+        tvTax.setText(String.valueOf(tax));
+        etTip.setText(String.valueOf(tip));
+        tvTotal.setText(String.valueOf(finalPrice));
+
         return checkoutFragment.getRoot();
     }
 }
