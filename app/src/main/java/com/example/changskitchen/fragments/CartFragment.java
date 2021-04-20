5f90676e4e7d3e9811d7757031005b6ece03cd8c
truonghh99 <truonghh@beloit.edu>
2021-04-20 14:00:23 -0700

    Display selected dishes in cart view

--- app/src/main/java/com/example/changskitchen/fragments/CartFragment.java
+++ app/src/main/java/com/example/changskitchen/fragments/CartFragment.java
@@ -3,6 +3,7 @@ package com.example.changskitchen.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
@@ -13,8 +14,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.changskitchen.R;
+import com.example.changskitchen.adapters.DishAdapter;
+import com.example.changskitchen.adapters.OrderItemAdapter;
 import com.example.changskitchen.databinding.FragmentCartBinding;
 import com.example.changskitchen.databinding.FragmentDishBinding;
+import com.example.changskitchen.models.Order;
+import com.example.changskitchen.models.OrderItem;
+import com.example.changskitchen.storage.CurrentOrder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,6 +36,8 @@ public class CartFragment extends Fragment {
     RecyclerView rvCartItems;
     TextView tvDate;
     Button btCheckout;
+    OrderItemAdapter adapter;
+
     public CartFragment() {
     }
 
@@ -53,6 +61,9 @@ public class CartFragment extends Fragment {
         tvDate = fragmentCartBinding.tvDate;
         btCheckout = fragmentCartBinding.btCheckout;
 
+        adapter = new OrderItemAdapter(getActivity(), CurrentOrder.orderItems);
+        rvCartItems.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvCartItems.setAdapter(adapter);
 
         return fragmentCartBinding.getRoot();
     }
