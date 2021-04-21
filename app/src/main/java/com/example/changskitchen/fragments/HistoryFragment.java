d055403da22338823ef4a92d89157dd70437c0ed
truonghh99 <truonghh@beloit.edu>
2021-04-21 13:07:01 -0700

    Implement History Fragment

--- app/src/main/java/com/example/changskitchen/fragments/HistoryFragment.java
+++ app/src/main/java/com/example/changskitchen/fragments/HistoryFragment.java
@@ -2,13 +2,30 @@ package com.example.changskitchen.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.changskitchen.R;
+import com.example.changskitchen.adapters.OrderAdapter;
+import com.example.changskitchen.databinding.FragmentHistoryBinding;
+import com.example.changskitchen.models.Order;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,29 +34,72 @@ import com.example.changskitchen.R;
  */
 public class HistoryFragment extends Fragment {
 
+    private static final String TAG = "HistoryFragment";
+    private List<Order> orderList = new ArrayList<>();
+    private RecyclerView rvOrders;
+    private OrderAdapter adapter;
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("server/saving-data/fireblog").child("orders");
+
     public HistoryFragment() {
-        // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
     public static HistoryFragment newInstance() {
         HistoryFragment fragment = new HistoryFragment();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        getAllOrder();
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+
+        FragmentHistoryBinding fragmentHistoryBinding = FragmentHistoryBinding.inflate(getLayoutInflater());
+
+        // Set up adapter & recycler view
+        rvOrders = fragmentHistoryBinding.rvOrders;
+        adapter = new OrderAdapter(getActivity(), orderList);
+        rvOrders.setLayoutManager(new LinearLayoutManager(getContext()));
+        rvOrders.setAdapter(adapter);
+
+        return fragmentHistoryBinding.getRoot();
+    }
+
+    public void getAllOrder() {
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final Query orders = ref.orderByChild("uid").equalTo(uid);
+
+        ChildEventListener orderListnener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Order order = snapshot.getValue(Order.class);
+                orderList.add(order);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        orders.addChildEventListener(orderListnener);
     }
 }
