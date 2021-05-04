0703cf7c8884f694fddb0a0826f57ab60069fe0b
truonghh99 <truonghh@beloit.edu>
2021-05-04 15:09:27 -0700

    Add contact fragment

--- app/src/main/java/com/example/changskitchen/fragments/ContactFragment.java
+++ app/src/main/java/com/example/changskitchen/fragments/ContactFragment.java
@@ -3,37 +3,49 @@ package com.example.changskitchen.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.changskitchen.R;
+import com.example.changskitchen.adapters.DishAdapter;
+import com.example.changskitchen.databinding.FragmentContactBinding;
+import com.example.changskitchen.databinding.FragmentMenuBinding;
+import com.example.changskitchen.models.Dish;
+import com.example.changskitchen.models.Menu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ContactFragment#newInstance} factory method to
+ * Use the {@link MenuFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ContactFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "ContactFragment";
+    private static final String ORDER_KEY = "OrderKey";
+    String orderId;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText etOrderId;
+    EditText etMessage;
+    Button btSend;
+    Button btCall;
 
     public ContactFragment() {
-        // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static ContactFragment newInstance() {
+    public static ContactFragment newInstance(String orderId) {
         ContactFragment fragment = new ContactFragment();
         Bundle args = new Bundle();
+        args.putString(ORDER_KEY, orderId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -42,15 +54,20 @@ public class ContactFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            orderId = getArguments().getString(ORDER_KEY);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        FragmentContactBinding fragmentContactBinding = FragmentContactBinding.inflate(getLayoutInflater());
+
+        etMessage = fragmentContactBinding.etMessage;
+        etOrderId = fragmentContactBinding.etOrderId;
+        btCall = fragmentContactBinding.btCall;
+        btSend = fragmentContactBinding.btSend;
+
+        return fragmentContactBinding.getRoot();
     }
 }
